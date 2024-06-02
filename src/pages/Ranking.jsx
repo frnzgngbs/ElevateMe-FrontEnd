d629068a34e9cb03a6accd66bf3b5aa2416f24ee
ErwinLambujon <your-email@example.com>
2024-06-02 16:49:18 +0800

    Latest Commit

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -226,7 +226,7 @@ const Ranking = () => {
 			},
 		});
 	};
-
+	console.log(initialSelectedValues);
 	const removeSelectedStatement = (index, id, venn, statement) => {
 		queuedProblemStatementDispatch({
 			type: PS_action.REMOVE_PROBLEM_STATEMENT,
