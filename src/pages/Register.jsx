c3be8fa97847f7f9279c320c2c3ccb383bd8054b
ErwinLambujon <erwinlambujon1@gmail.com>
2024-10-18 16:42:36 +0800

    Updated Login and Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,210 +8,240 @@ import Grid from "@mui/material/Grid";
 import useTheme from "@mui/material/styles/useTheme";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 const Register = () => {
-	var theme = useTheme();
-	const [userData, setUserData] = useState({
-		username: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-	});
+  const theme = useTheme();
 
-	const handleChange = (e) => {
-		setUserData((prev) => ({
-			...prev,
-			[e.target.name]: e.target.value,
-		}));
-	};
+  const [userData, setUserData] = useState({
+    email: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+    confirmPassword: "",
+    user_type: "STUDENT",
+  });
 
-	const handleRegistration = async (e) => {
-		e.preventDefault();
+  const handleChange = (e) => {
+    setUserData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-		if (userData.password !== userData.confirmPassword) {
-			alert("Passwords don't match.");
-			setUserData((prev) => ({
-				...prev,
-				password: "",
-				confirmPassword: "",
-			}));
-			return;
-		}
+  const handleRegistration = async (e) => {
+    e.preventDefault();
 
-		try {
-			let response = await axios.post("http://localhost:8000/api/user/", {
-				...userData,
-			});
+    if (userData.password !== userData.confirmPassword) {
+      alert("Passwords does not match.");
+      setUserData((prev) => ({
+        ...prev,
+        password: "",
+        confirmPassword: "",
+      }));
+      return;
+    }
 
-			console.log(response);
+    try {
+      let response = await axios.post("http://localhost:8000/api/user/", {
+        email: userData.email,
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        password: userData.password,
+        user_type: userData.user_type,
+      });
 
-			if (response.status === 201) {
-				alert("Created successfully!");
-				setUserData({
-					username: "",
-					email: "",
-					password: "",
-					confirmPassword: "",
-				});
-			}
-		} catch (err) {
-			console.error(err);
+      if (response.status === 201) {
+        alert("User created successfully!");
+        setUserData({
+          email: "",
+          first_name: "",
+          last_name: "",
+          password: "",
+          confirmPassword: "",
+          user_type: "STUDENT",
+        });
+      }
+    } catch (err) {
+      console.error(err);
 
-			if (err.response && err.response.status == 400) {
-				const { data } = err.response;
+      if (err.response && err.response.status === 400) {
+        const { data } = err.response;
+        if (data.email) {
+          alert(data.email[0]);
+          setUserData((prev) => ({
+            ...prev,
+            email: "",
+          }));
+        }
+      }
+    }
+  };
 
-				if (data.status === 400) {
-					if (data.username) {
-						alert(data.username[0]);
-						setUserData((prev) => ({
-							...prev,
-							username: "",
-						}));
-					} else if (data.email) {
-						setUserData((prev) => ({
-							...prev,
-							email: "",
-						}));
-						alert(data.email[0]);
-					}
-				}
-			}
-		}
-	};
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
 
-	useEffect(() => {
-		console.log(userData);
-	}, [userData]);
+  const inputProps = {
+    sx: {
+      borderRadius: "50px",
+      "& fieldset": { borderColor: "stroke.main" },
+    },
+  };
 
-	const inputProps = {
-		sx: {
-			borderRadius: "50px",
-			"& fieldset": { borderColor: "stroke.main" },
-		},
-	};
+  const inputSx = {
+    boxSizing: "border-box",
+    width: "100%",
+  };
 
-	const inputSx = {
-		boxSizing: "border-box",
-		width: "100%",
-	};
+  const inputFieldContainer = {
+    paddingTop: "15px",
+    paddingBottom: "15px",
+  };
 
-	const inputFieldContainer = {
-		paddingTop: "15px",
-		paddingBottom: "15px",
-	};
-
-	return (
-		<Grid
-			container
-			spacing={0}
-			direction="row"
-			alignItems="center"
-			justifyContent="center"
-			sx={{ minHeight: "100vh" }}>
-			<Grid item xs={11} md={5} xl={5}>
-				<Paper
-					elevation={0}
-					sx={{
-						borderRadius: "32px",
-						paddingTop: "50px",
-						paddingBottom: "50px",
-						userSelect: "none",
-						textAlign: "center",
-					}}>
-					<h1
-						style={{
-							margin: 0,
-							color: theme.palette.primary.main,
-						}}>
-						ElevateMe
-					</h1>
-					<form onSubmit={handleRegistration}>
-						<FormControl
-							sx={{
-								width: "100%",
-								boxSizing: "border-box",
-								paddingLeft: "5rem",
-								paddingRight: "5rem",
-							}}>
-							<Box sx={inputFieldContainer}>
-								<TextField
-									InputProps={inputProps}
-									sx={inputSx}
-									placeholder="Username"
-									type="text"
-									name="username"
-									id="username"
-									value={userData.username}
-									onChange={handleChange}
-								/>
-							</Box>
-							<Box sx={inputFieldContainer}>
-								<TextField
-									InputProps={inputProps}
-									sx={inputSx}
-									placeholder="Email"
-									type="email"
-									name="email"
-									id="email"
-									value={userData.email}
-									onChange={handleChange}
-								/>
-							</Box>
-							<Box sx={inputFieldContainer}>
-								<TextField
-									InputProps={inputProps}
-									sx={inputSx}
-									placeholder="Password"
-									type="password"
-									name="password"
-									id="password"
-									value={userData.password}
-									onChange={handleChange}
-								/>
-							</Box>
-							<Box sx={inputFieldContainer}>
-								<TextField
-									InputProps={inputProps}
-									sx={inputSx}
-									placeholder="Confirm password"
-									type="password"
-									name="confirmPassword"
-									id="confirmPassword"
-									value={userData.confirmPassword}
-									onChange={handleChange}
-								/>
-							</Box>
-							<Button
-								type="submit"
-								variant="contained"
-								sx={{
-									borderRadius: "50rem",
-									width: "100%",
-									boxSizing: "border-box",
-									height: "4rem",
-									marginTop: "3rem",
-									fontSize: "1.2rem",
-									fontWeight: "bold",
-								}}>
-								Sign in
-							</Button>
-						</FormControl>
-					</form>
-					<Box sx={{ marginTop: "1rem" }}>
-						Already have an account?{" "}
-						<Link
-							to="/login"
-							style={{
-								color: "primary.main",
-								textDecoration: "none",
-							}}>
-							Log in
-						</Link>
-					</Box>
-				</Paper>
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={11} md={5} xl={5}>
+        <Paper
+          elevation={0}
+          sx={{
+            borderRadius: "32px",
+            paddingTop: "50px",
+            paddingBottom: "50px",
+            userSelect: "none",
+            textAlign: "center",
+          }}
+        >
+          <h1
+            style={{
+              margin: 0,
+              color: theme.palette.primary.main,
+            }}
+          >
+            ElevateMe
+          </h1>
+          <form onSubmit={handleRegistration}>
+            <FormControl
+              sx={{
+                width: "100%",
+                boxSizing: "border-box",
+                paddingLeft: "5rem",
+                paddingRight: "5rem",
+              }}
+            >
+              <Box sx={inputFieldContainer}>
+                <TextField
+                  InputProps={inputProps}
+                  sx={inputSx}
+                  placeholder="First Name"
+                  type="text"
+                  name="first_name"
+                  value={userData.first_name}
+                  onChange={handleChange}
+                  required
+                />
+              </Box>
+              <Box sx={inputFieldContainer}>
+                <TextField
+                  InputProps={inputProps}
+                  sx={inputSx}
+                  placeholder="Last Name"
+                  type="text"
+                  name="last_name"
+                  value={userData.last_name}
+                  onChange={handleChange}
+                  required
+                />
+              </Box>
+              <Box sx={inputFieldContainer}>
+                <TextField
+                  InputProps={inputProps}
+                  sx={inputSx}
+                  placeholder="Email"
+                  type="email"
+                  name="email"
+                  value={userData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </Box>
+              <Box sx={inputFieldContainer}>
+                <TextField
+                  InputProps={inputProps}
+                  sx={inputSx}
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  value={userData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </Box>
+              <Box sx={inputFieldContainer}>
+                <TextField
+                  InputProps={inputProps}
+                  sx={inputSx}
+                  placeholder="Confirm Password"
+                  type="password"
+                  name="confirmPassword"
+                  value={userData.confirmPassword}
+                  onChange={handleChange}
+                  required
+                />
+              </Box>
+              <Box sx={inputFieldContainer}>
+                <Select
+                  name="user_type"
+                  value={userData.user_type}
+                  onChange={handleChange}
+                  displayEmpty
+                  sx={{ width: "100%" }}
+                >
+                  <MenuItem value="STUDENT">Student</MenuItem>
+                  <MenuItem value="TEACHER">Teacher</MenuItem>
+                </Select>
+              </Box>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  borderRadius: "50rem",
+                  width: "100%",
+                  boxSizing: "border-box",
+                  height: "4rem",
+                  marginTop: "3rem",
+                  fontSize: "1.2rem",
+                  fontWeight: "bold",
+                }}
+              >
+                Sign up
+              </Button>
+            </FormControl>
+          </form>
+          <Box sx={{ marginTop: "1rem" }}>
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              style={{
+                color: "primary.main",
+                textDecoration: "none",
+              }}
+            >
+              Log in
+            </Link>
+          </Box>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Register;
