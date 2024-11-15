4317e520719bf7f2a10b580e8f627c57d8a06627
Franz Genegobis <genegobis.franz@gmail.com>
2024-11-16 01:07:37 +0800

    Modularized get currently login

--- src/components/UserAppbar.jsx
+++ src/components/UserAppbar.jsx
@@ -6,173 +6,154 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { Avatar, IconButton, Menu, MenuItem, Divider } from "@mui/material";
 import useAuth from "../hooks/useAuth";
 import axios from "axios";
-import { API_BASE_URL } from '../helpers/constant';
-
+import { API_BASE_URL } from "../helpers/constant";
 
 const UserAppbar = () => {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const { Logout } = useAuth();
-    const open = Boolean(anchorEl);
-    const navigate = useNavigate();
-    const [user, setUser] = useState(null);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const { Logout } = useAuth();
+	const open = Boolean(anchorEl);
+	const navigate = useNavigate();
+	const [user, setUser] = useState(null);
+	const { getCurrentlyLogin } = useAuth();
 
-    const menuItems = {
-        home: "Home",
-        venn: "Venn",
-        saved: "Saved",
-        rank: "Ranking",
-        five_whys: "Five-Whys",
-        hmw: "How Might We",
-        report: "Report",
-        
-    };
+	const menuItems = {
+		home: "Home",
+		venn: "Venn",
+		saved: "Saved",
+		rank: "Ranking",
+		five_whys: "Five-Whys",
+		hmw: "How Might We",
+		report: "Report",
+	};
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const token = localStorage.getItem("token");
+	useEffect(() => {
+		const fetchUser = async () => {
+			const loginUser = await getCurrentlyLogin();
+			setUser(loginUser);
+		};
+		fetchUser();
+	}, [getCurrentlyLogin]);
 
-                if (!token) {
-                    throw new Error("No token found. Please log in.");
-                }
-                const response = await axios.get(`${API_BASE_URL}/api/user/get_currently_login/`, {
-                    headers: {
-                        Authorization: `Token ${token}`,
-                    },
-                });
-                setUser(response.data);
-            } catch (error) {
-                console.error("Error fetching user info:", error);
-            }
-        };
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-        fetchUser();
-    }, []);
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+	const handleLogoClick = () => {
+		navigate("/home", { replace: true });
+	};
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+	const handleMenuOptionClick = (path) => {
+		navigate(`/${path}`, { replace: true });
+		handleClose();
+	};
 
-    const handleLogoClick = () => {
-        navigate("/home", { replace: true });
-    };
+	return (
+		<Box>
+			<AppBar
+				position="static"
+				elevation={2}
+				sx={{
+					bgcolor: "white",
+					boxShadow: "0px 2px 4px rgba(0, 0, 0, 0)",
+					marginBottom: 3,
+				}}>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+						px: 4,
+						py: 2,
+					}}>
+					<Typography
+						variant="h4"
+						onClick={handleLogoClick}
+						sx={{
+							cursor: "pointer",
+							fontWeight: "bold",
+							color: "#186F65",
+							"&:hover": { color: "#165" },
+						}}>
+						ElevateMe
+					</Typography>
 
-    const handleMenuOptionClick = (path) => {
-        navigate(`/${path}`, { replace: true });
-        handleClose();
-    };
+					{user && (
+						<Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+							<Box sx={{ textAlign: "right" }}>
+								<Typography
+									variant="body1"
+									sx={{ fontWeight: 500, color: "#444", marginBottom: -0.5 }}>
+									{user.first_name} {user.last_name}
+								</Typography>
+								<Typography variant="caption" sx={{ color: "#666" }}>
+									{user.user_type === "STUDENT" ? "Student" : "Teacher"}
+								</Typography>
+							</Box>
+							<IconButton
+								onClick={handleClick}
+								sx={{
+									"&:hover": { bgcolor: "#f0f0f0" },
+									transition: "background-color 0.3s",
+								}}>
+								<Avatar
+									sx={{
+										bgcolor: "#186F65",
+										color: "white",
+										fontWeight: "bold",
+									}}
+									alt={user.first_name}>
+									{user.first_name.charAt(0).toUpperCase()}
+								</Avatar>
+							</IconButton>
+						</Box>
+					)}
 
-    return (
-        <Box>
-            <AppBar position="static" elevation={2} sx={{ bgcolor: "white", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0)", marginBottom:3 }} >
-                <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        px: 4,
-                        py: 2,
-                    }}
-                >
-                    <Typography
-                        variant="h4"
-                        onClick={handleLogoClick}
-                        sx={{ 
-                            cursor: "pointer", 
-                            fontWeight: "bold", 
-                            color: "#186F65",
-                            "&:hover": { color: "#165" },
-                        }}
-                    >
-                        ElevateMe
-                    </Typography>
-                    
-                    {user && (
-                        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                            <Box sx={{ textAlign: "right"  }}>
-                                <Typography 
-                                    variant="body1" 
-                                    sx={{ fontWeight: 500, color: "#444", marginBottom:-.5 }}
-                                >
-                                    {user.first_name} {user.last_name}
-                                </Typography>
-                                <Typography 
-                                    variant="caption" 
-                                    sx={{ color: "#666" }}
-                                >
-                                    {user.user_type === "STUDENT" ? "Student" : "Teacher"}
-                                </Typography>
-                            </Box>
-                            <IconButton 
-                                onClick={handleClick} 
-                                sx={{
-                                    "&:hover": { bgcolor: "#f0f0f0" },
-                                    transition: "background-color 0.3s",
-                                }}
-                            >
-                                <Avatar 
-                                    sx={{ 
-                                        bgcolor: "#186F65", 
-                                        color: "white",
-                                        fontWeight: "bold",
-                                    }} 
-                                    alt={user.first_name}
-                                >
-                                    {user.first_name.charAt(0).toUpperCase()}
-                                </Avatar>
-                            </IconButton>
-                        </Box>
-                    )}
-
-                    <Menu
-                        id="avatar-menu"
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        MenuListProps={{
-                            "aria-labelledby": "avatar-menu-button",
-                        }}
-                        sx={{ mt: 1 }}
-                    >
-                        {Object.entries(menuItems).map(([key, value]) => (
-                            <MenuItem
-                                key={key}
-                                onClick={() =>
-                                    value === "Log Out" ? Logout() : handleMenuOptionClick(key)
-                                }
-                                sx={{
-                                    "&:hover": {
-                                        backgroundColor: "#f4f4f4",
-                                    },
-                                    px: 3,
-                                    py: 1,
-                                }}
-                            >
-                                {value}
-                            </MenuItem>
-                        ))}
-                        <Divider />
-                        <MenuItem 
-                            onClick={Logout} 
-                            sx={{ 
-                                color: "#d32f2f", 
-                                "&:hover": { backgroundColor: "#ffebee" },
-                                px: 3,
-                                py: 1,
-                            }}
-                        >
-                            Log Out
-                        </MenuItem>
-                    </Menu>
-                </Box>
-            </AppBar>
-            <Outlet />
-        </Box>
-    );
+					<Menu
+						id="avatar-menu"
+						anchorEl={anchorEl}
+						open={open}
+						onClose={handleClose}
+						MenuListProps={{
+							"aria-labelledby": "avatar-menu-button",
+						}}
+						sx={{ mt: 1 }}>
+						{Object.entries(menuItems).map(([key, value]) => (
+							<MenuItem
+								key={key}
+								onClick={() =>
+									value === "Log Out" ? Logout() : handleMenuOptionClick(key)
+								}
+								sx={{
+									"&:hover": {
+										backgroundColor: "#f4f4f4",
+									},
+									px: 3,
+									py: 1,
+								}}>
+								{value}
+							</MenuItem>
+						))}
+						<Divider />
+						<MenuItem
+							onClick={Logout}
+							sx={{
+								color: "#d32f2f",
+								"&:hover": { backgroundColor: "#ffebee" },
+								px: 3,
+								py: 1,
+							}}>
+							Log Out
+						</MenuItem>
+					</Menu>
+				</Box>
+			</AppBar>
+			<Outlet />
+		</Box>
+	);
 };
 
 export default UserAppbar;
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,65 +1,84 @@
 import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { API_BASE_URL } from '../helpers/constant';
-
+import { API_BASE_URL } from "../helpers/constant";
 
 const useAuth = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const Login = async (user) => {
-    if (!user.email || !user.password) {
-      alert("Please enter both email and password");
-      return;
-    }
-    try {
-      /*console.log("Login attempt with payload:", {
+	const Login = async (user) => {
+		if (!user.email || !user.password) {
+			alert("Please enter both email and password");
+			return;
+		}
+		try {
+			/*console.log("Login attempt with payload:", {
         email: user.email,
         password: user.password,
       });*/
 
-      let response = await axios.post(`${API_BASE_URL}/api/user/login/`, {
-        email: user.email,
-        password: user.password,
-      });
+			let response = await axios.post(`${API_BASE_URL}/api/user/login/`, {
+				email: user.email,
+				password: user.password,
+			});
 
-      if (response.status === 200) {
-        localStorage.setItem("token", response.data.token);
-        // alert("Login successfully!");
-        navigate("/home", { replace: true });
-      }
-    } catch (err) {
-      if (err.code === "ERR_BAD_REQUEST") {
-        alert(err.response.data.error);
-      } else {
-        alert("Not connected with the back-end server");
-      }
-    }
-  };
+			if (response.status === 200) {
+				localStorage.setItem("token", response.data.token);
+				// alert("Login successfully!");
+				navigate("/home", { replace: true });
+			}
+		} catch (err) {
+			if (err.code === "ERR_BAD_REQUEST") {
+				alert(err.response.data.error);
+			} else {
+				alert("Not connected with the back-end server");
+			}
+		}
+	};
 
-  const Logout = async () => {
-    try {
-      let token = localStorage.getItem("token");
+	const Logout = async () => {
+		try {
+			let token = localStorage.getItem("token");
 
-      await axios.post(
-        `${API_BASE_URL}/api/user/logout/`,
-        {},
-        {
-          headers: { Authorization: `Token ${token}` },
-        }
-      );
+			await axios.post(
+				`${API_BASE_URL}/api/user/logout/`,
+				{},
+				{
+					headers: { Authorization: `Token ${token}` },
+				}
+			);
 
-      localStorage.removeItem("token");
-      sessionStorage.clear();
-  
+			localStorage.removeItem("token");
+			sessionStorage.clear();
 
-      navigate("/login", { replace: true });
-    } catch (err) {
-      // console.log(err);
-    }
-  };
+			navigate("/login", { replace: true });
+		} catch (err) {
+			// console.log(err);
+		}
+	};
 
-  return { Login, Logout };
+	const getCurrentlyLogin = async () => {
+		try {
+			const token = localStorage.getItem("token");
+
+			if (!token) {
+				throw new Error("No token found. Please log in.");
+			}
+			const response = await axios.get(
+				`${API_BASE_URL}/api/user/get_currently_login/`,
+				{
+					headers: {
+						Authorization: `Token ${token}`,
+					},
+				}
+			);
+			return response.data;
+		} catch (error) {
+			console.error("Error fetching user info:", error);
+		}
+	};
+
+	return { Login, Logout, getCurrentlyLogin };
 };
 
 export default useAuth;
